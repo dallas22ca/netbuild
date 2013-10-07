233933549cba8e5fd578ef5ba0a5a101b81eae1c
Dallas Read <dallas@excitereative.ca>
2013-10-07 08:47:24 -0300

    back

--- config/recipes/assets.rb
+++ config/recipes/assets.rb
@@ -1,18 +1,18 @@
 #Doesn't Precompile all the assets if there are no changes.....
 
-namespace :deploy do
-  namespace :assets do
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      begin
-        from = source.next_revision(current_revision)
-      rescue
-        err_no = true
-      end
-      if err_no || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-      else
-        logger.info "Skipping asset pre-compilation because there were no asset changes"
-      end
-   end
-  end
-end
+# namespace :deploy do
+#   namespace :assets do
+#     task :precompile, :roles => :web, :except => { :no_release => true } do
+#       begin
+#         from = source.next_revision(current_revision)
+#       rescue
+#         err_no = true
+#       end
+#       if err_no || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+#         run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+#       else
+#         logger.info "Skipping asset pre-compilation because there were no asset changes"
+#       end
+#    end
+#   end
+# end
