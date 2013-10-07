b78ab04270e5c2c41d9e93270658b26b97ec51ab
Dallas Read <dallas@excitereative.ca>
2013-10-07 09:31:41 -0300

    fix assets.

--- config/recipes/assets.rb
+++ config/recipes/assets.rb
@@ -1,18 +1,21 @@
-#Doesn't Precompile all the assets if there are no changes.....
-
-# namespace :deploy do
-#   namespace :assets do
-#     task :precompile, :roles => :web, :except => { :no_release => true } do
-#       begin
-#         from = source.next_revision(current_revision)
-#       rescue
-#         err_no = true
-#       end
-#       if err_no || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-#         run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-#       else
-#         logger.info "Skipping asset pre-compilation because there were no asset changes"
-#       end
-#    end
-#   end
-# end
+namespace :assets do
+  task :precompile, :roles => :web, :except => { :no_release => true } do
+    # Check if assets have changed. If not, don't run the precompile task - it takes a long time.
+    force_compile = false
+    changed_asset_count = 0
+    begin
+      from = source.next_revision(current_revision)
+      asset_locations = 'app/assets/ lib/assets vendor/assets'
+      changed_asset_count = capture("cd #{latest_release} && #{source.local.log(from)} #{asset_locations} | wc -l").to_i
+    rescue Exception => e
+      logger.info "Error: #{e}, forcing precompile"
+      force_compile = false
+    end
+    if changed_asset_count > 0 || force_compile
+      logger.info "#{changed_asset_count} assets have changed. Pre-compiling"
+      run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+    else
+      logger.info "#{changed_asset_count} assets have changed. Skipping asset pre-compilation"
+    end
+  end
+end
