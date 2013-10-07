6997f9f6a4cf8a396d2ddb825e09293c360bec92
Dallas Read <dallas@excitereative.ca>
2013-10-07 09:22:31 -0300

    production file?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,15 +28,9 @@ Netbuild::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
-  # Generate digests for assets URLs.
+  config.assets.compress = true
   config.assets.digest = true
-
-  # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
-
-  # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
