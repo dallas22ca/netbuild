31a95237b8f8250ca90e65fb72d424ff60b2c437
Dallas Read <dallas@excitereative.ca>
2013-10-08 20:05:18 -0300

    set home.

--- app/models/website.rb
+++ app/models/website.rb
@@ -37,6 +37,8 @@ class Website < ActiveRecord::Base
         document_id: theme.default_document
       )
       
+      self.update_attributes :home_id, home.id
+      
       sign_in = pages.create(
         title: "Sign In",
         permalink: "sign_in",
