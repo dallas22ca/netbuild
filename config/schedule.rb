93d0fe6f5db36da676dc5e754368001a69b349a5
Dallas Read <dallas@excitereative.ca>
2013-10-09 22:15:36 -0300

    new crontab

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 set :output, "/home/deployer/apps/eo/current/log/cron.log"
 
-every :day, at: "4:00 AM" do
-  runner "Invoice.add_addons_to_invoices"
-end
+# every :day, at: "4:00 AM" do
+#   runner "Invoice.add_addons_to_invoices"
+# end
