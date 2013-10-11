d534856734231c27082409c372c03aa63844ca8c
Dallas Read <dallas@excitereative.ca>
2013-10-11 15:12:18 -0300

    Symlink config file in.

--- config/recipes/postgresql.rb
+++ config/recipes/postgresql.rb
@@ -38,6 +38,7 @@ namespace :postgresql do
   desc "Symlink the database.yml file into latest release"
   task :symlink, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/config.yml #{release_path}/config/config.yml"
   end
   after "deploy:finalize_update", "postgresql:symlink"
   
