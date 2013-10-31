2f012ebd7bd14a41d0973d606d9f9aed8892d792
Dallas Read <dallas@excitereative.ca>
2013-10-31 13:26:03 -0300

    real migrations

--- db/migrate/20131031114737_add_membership_to_invoices.rb
+++ db/migrate/20131031114737_add_membership_to_invoices.rb
@@ -6,7 +6,7 @@ class AddMembershipToInvoices < ActiveRecord::Migration
     add_column :memberships, :last_4, :integer
     add_column :websites, :stripe_access_token, :string
     add_column :websites, :stripe_user_id, :string
-    # add_column :invoices, :netbuild_website_id, :integer
-    # add_index :invoices, :netbuild_website_id
+    add_column :invoices, :netbuild_website_id, :integer
+    add_index :invoices, :netbuild_website_id
   end
 end
