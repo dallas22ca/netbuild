41cc502da9d5fa5a9ac1ab70ded548397882993e
Dallas Read <dallas@excitereative.ca>
2013-11-08 14:43:28 -0400

    Lame migration.

--- db/migrate/20131108155525_add_published_to_pages.rb
+++ db/migrate/20131108155525_add_published_to_pages.rb
@@ -1,6 +1,5 @@
 class AddPublishedToPages < ActiveRecord::Migration
   def change
     add_column :pages, :published, :boolean, default: false
-    add_column :pages, :published_at, :datetime
   end
 end
--- db/migrate/20131108174042_add_children_have_dates_to_pages.rb
+++ db/migrate/20131108174042_add_children_have_dates_to_pages.rb
@@ -1,5 +1,6 @@
 class AddChildrenHaveDatesToPages < ActiveRecord::Migration
   def change
     add_column :pages, :children_have_dates, :boolean, default: false
+    add_column :pages, :published_at, :datetime
   end
 end
