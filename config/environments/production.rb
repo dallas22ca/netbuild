6cc8f7deaf9da17f290c4f9036fdba52df664245
Dallas Read <dallas@excitereative.ca>
2013-11-15 12:07:41 -0400

    urg.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,13 +25,21 @@ Netbuild::Application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
+  
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
+
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-  config.assets.compress = true
-  config.assets.digest = true
   config.assets.version = '1.0'
-  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
