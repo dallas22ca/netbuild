a50394c430dec7eb2c9c4ac3b78e70c7944b5af6
Dallas Read <dallas@excitereative.ca>
2013-11-22 08:45:12 -0400

    straggling commas.

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -130,9 +130,9 @@ class WebsitesController < ApplicationController
     
     def website_params
       if super_admin?
-        params.require(:website).permit(:title, :domain, :theme_id, :duplicate_theme, :home_id, :primary_colour, :secondary_colour, :header, :warnings, :card_token, :customer_token, :stripe_access_token, :stripe_user_id, :address, :invoice_blurb, , :include_js, addon_ids: [])
+        params.require(:website).permit(:title, :domain, :theme_id, :duplicate_theme, :home_id, :primary_colour, :secondary_colour, :header, :warnings, :card_token, :customer_token, :stripe_access_token, :stripe_user_id, :address, :invoice_blurb, :include_js, addon_ids: [])
       elsif @website.adminable_by(current_user)
-        params.require(:website).permit(:title, :domain, :theme_id, :duplicate_theme, :home_id, :primary_colour, :secondary_colour, :header, :warnings, :card_token, :stripe_access_token, :stripe_user_id, :address, :invoice_blurb, , :include_js, addon_ids: [])
+        params.require(:website).permit(:title, :domain, :theme_id, :duplicate_theme, :home_id, :primary_colour, :secondary_colour, :header, :warnings, :card_token, :stripe_access_token, :stripe_user_id, :address, :invoice_blurb, :include_js, addon_ids: [])
       else
         params.require(:website).permit(:title, :domain, :theme_id, :duplicate_theme, :home_id, :primary_colour, :secondary_colour, :header, :warnings, :include_js)
       end
