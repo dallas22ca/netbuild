f72cc4c09cbd6772e3a4d4a0a57357a4e9f1896b
Dallas Read <dallas@excitereative.ca>
2013-11-25 12:40:23 -0400

    aha.

--- config/recipes/faye.rb
+++ config/recipes/faye.rb
@@ -1,17 +1,17 @@
 namespace :faye do
   desc "Start Faye"
   task :start, :roles => :app do
-    run "cd #{current_path}; thin start -C config/thin.yml"
+    run "cd #{current_path}; thin start -C config/faye.yml"
   end
 
   desc "Stop Faye"
   task :stop, :roles => :app do
-    run "cd #{current_path}; thin stop -C config/thin.yml"
+    run "cd #{current_path}; thin stop -C config/faye.yml"
   end
 
   desc "Restart Faye"
   task :restart, :roles => :app do
-    run "cd #{current_path}; thin restart -C config/thin.yml"
+    run "cd #{current_path}; thin restart -C config/faye.yml"
   end
 end
 
