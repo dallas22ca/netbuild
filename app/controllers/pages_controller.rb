4a3a32a34cb25ae33aa8ca69f19eab956942d488
Dallas Read <dallas@excitereative.ca>
2014-01-03 17:08:59 -0400

    Parse dates properly.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -27,7 +27,7 @@ class PagesController < ApplicationController
       
       @page = @website.pages.where(permalink: params[:a]).first
     elsif params[:post]
-      start = Time.parse("#{params[:year]}/#{params[:month]}")
+      start = Time.parse("#{params[:year].to_i}/#{params[:month].to_i}")
       finish = start.end_of_month
       
       if params[:permalink]
@@ -39,11 +39,11 @@ class PagesController < ApplicationController
       @page = @parent.children.where("permalink = ? and published_at >= ? and published_at <= ?", params[:post], start, finish).first if @parent
     elsif params[:month]
       @page = @website.pages.dated.where(permalink: params[:permalink]).first
-      @month = params[:month]
-      @year = params[:year]
+      @month = params[:month].to_i
+      @year = params[:year].to_i
     elsif params[:year]
       @page = @website.pages.dated.where(permalink: params[:permalink]).first
-      @year = params[:year]
+      @year = params[:year].to_i
     elsif params[:c]
       @grandparent = @website.pages.roots.not_dated.where(permalink: params[:a]).first
       @parent = @grandparent.children.not_dated.where(permalink: params[:b]).first
